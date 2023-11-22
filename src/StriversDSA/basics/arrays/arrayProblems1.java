d7cd0639591884ba7e634198143bfa1a4a54f93b
sourabh agari <sourabh.agari@optum.com>
2023-11-22 14:19:33 +0530

    Restructured

---
+++ src/StriversDSA/basics/arrays/arrayProblems1.java
@@ -0,0 +1,15 @@
+package StriversDSA.basics.arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+/*
+Arrays Problems:
+*/
+public class arrayProblems1 {
+
+    public static void main(String[] args) {
+
+    }
+}
